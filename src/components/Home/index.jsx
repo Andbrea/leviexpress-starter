b496007155d11154ff6c5a003bc379e5b24a0e14
Andbrea <a.bravencovaa@gmail.com>
2021-05-29 10:39:09 +0200

    routovani hotovo

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { JourneyPicker } from '../JourneyPicker';
 
 export const Home = () => (
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+const Reservation = () => {
+    return (
+<h2>Neco se deje</h2>
+)
+}
+export default Reservation;
